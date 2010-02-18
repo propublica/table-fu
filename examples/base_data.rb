8dda22e83ef16f4a5a351e349bfe3e331af5111a
Jeff Larson <thejefflarson@gmail.com>
2010-02-18 16:10:19 -0500

    adding doc stuff

---
+++ examples/base_data.rb
@@ -0,0 +1,6 @@
+csv =<<-CSV
+Project,Cost,Date,URL
+Build Supercollider,500_000_000.50,09/15/2009,http://project.com
+Harness Power of Fusion,25_000_000,09/16/2009,http://project2.com
+Motorized Bar Stool,45.00,09/17/2009,http://project3.com
+CSV
