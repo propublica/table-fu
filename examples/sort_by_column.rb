3bdb1cd064207da9b106531cb9d108408780c2a7
Jeff Larson <thejefflarson@gmail.com>
2010-02-18 17:28:58 -0500

    docs and examples

---
+++ examples/sort_by_column.rb
@@ -0,0 +1,14 @@
+require 'table_fu'
+require 'FasterCSV'
+
+csv =<<-CSV
+Author,Best Book,Number of Pages,Style
+Samuel Beckett,Malone Muert,120,Modernism
+James Joyce,Ulysses,644,Modernism
+Nicholson Baker,Mezannine,150,Minimalism
+Vladimir Sorokin,The Queue,263,Satire
+CSV
+
+@spreadsheet = TableFu.new(FasterCSV.parse(csv)) do |s|
+  s.sorted_by = {'Best Book' => {'order' => 'ascending'}}
+end
